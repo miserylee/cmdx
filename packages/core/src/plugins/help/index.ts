d47cf47579b050146e38223be0bec391c4876fc9
MiseryLee <miserylee@foxmail.com>
2024-10-12 19:40:45 +0800

    fix: help plugin set at beforeExecute hook

--- packages/core/src/plugins/help/index.ts
+++ packages/core/src/plugins/help/index.ts
@@ -5,7 +5,7 @@ import { type Plugin } from '../../helpers/types';
 export const help: Plugin = {
   name: 'help',
   apply: (hooks) => {
-    hooks.afterParse(async (context) => {
+    hooks.beforeExecute(async (context) => {
       if (context.unknownShortenFlags.h || context.unknownFlags.help) {
         context.allowInvalidInputs = true;
         context.action = path.resolve(__dirname, 'helpAction');
